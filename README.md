0f317ef38fdcf0d298deae687ce72eab19f57736
Renwick Young <young.renwick@gmail.com>
2018-01-17 14:32:08 -0800

    initial

---
+++ README.md
@@ -0,0 +1,14 @@
+Getting started'
+
+# Clone repo:
+git clone git@github.com:ryoung18/centron.git
+
+cd centron
+
+
+
+
+//
+npm install -g create-react-app
+create-react-app centron
+cd centron
---
+++ src/Header.js
@@ -0,0 +1,11 @@
+import React,  { Component } from 'react';
+import "./Header.css";
+
+
+const Header = () => {
+  <header className="logo slide-down">
+   Centron  <img src="../public/images/centron-logo.png" /> Life Products
+ </header>
+}
+
+export default Header;
