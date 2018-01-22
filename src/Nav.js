89147a957d0822341093f00e8dd7edc48175a6c5
Renwick Young <young.renwick@gmail.com>
2018-01-21 21:36:01 -0800

    navbar search

--- src/Nav.js
+++ src/Nav.js
@@ -8,28 +8,48 @@ class Nav extends Component {
     super(props)
     this.state = {
       showMenu: 0,
-      display: 'search',
+      searchInput: '',
+      display: 'default',
       hrefInfo: 'hello'
     }
 
     this.handleClick = this.handleClick.bind(this);
+    this.handleChange = this.handleChange.bind(this);
     this.generateHref = this.generateHref.bind(this);
   }
 
+  handleChange(event) {
+    _setState(this, {searchInput: event.target.value})
+    console.log(this.state.searchInput)
+  }
+
+  focusTextInput() {
+    // Explicitly focus the text input using the raw DOM API
+    this.textInput.focus();
+  }
+
+
   handleClick(event) {
     let {id} = event.currentTarget;
-    if(id === 'menu') {
-      _setState(this, {cshowMenu: this.state.showMenu+1} )
 
-      if(this.state.showMenu === 1 ) {
+    if(id === 'menu') {
+      _setState(this, {showMenu: this.state.showMenu+1});
+      if(this.state.showMenu === 1) {
         setStateOnTimeOut(this, {showMenu: 0})
-      }
+      };
+      return;
     }
+
+    if(id === 'search') {
+      _setState(this, {display: id});
+      return;
+    };
+
+    _setState(this, {display: 'default', searchInput: ''} )
+
   }
 
   generateHref(type) {
-    console.log('lele', type)
-
     let result;
     // <a href={`https://twitter.com/intent/tweet?text=Centron%20Life%20Products%20-%20Clavicle%20Supprt%20On%20Sale&url=https%3A%2F%2Fwww.centronlp.com%2Fclavicle%2Fhand&tsrc=twtr`}>
     switch (type) {
@@ -43,62 +63,48 @@ class Nav extends Component {
     }
 
     return result;
-
-
   }
 
   render() {
-    let navItemCss = {
-      default: '',
-      search: ''
-    }
+    let {display} = this.state;
+    let navItemDisplay;
 
-    let navItems = {
-      default: [
-        {svg: 'menu-icon.svg', id:'menu'},
-        {svg: 'home-icon.svg', id:'home'},
-        {svg: 'search-icon.svg', id:'search'},
-        {svg: 'email-icon.svg', id:'email', css:'circle-btn', href: true },
-        {svg: 'twitter-icon.svg', id:'twitter', css:'circle-btn', href: true },
-        {svg: 'facebook-icon.svg', id:'facebook', css:'circle-btn'},
-        {svg: 'whatsapp-icon.svg', id:'whatsapp', css:'circle-btn'}
-      ],
-      search: [
-        {svg: 'search-icon.svg', id:'search icon'},
-        {input: true, id: 'input' },
-        {button: 'Done', id: 'button'}
-      ]
-    }
-
-    let navItemDisplay = navItems[ this.state['display'] ].map(item => {
-      let context = this;
-
-      let something = {
-        svg: ( <img src={`svgs/${item.svg}`} className={item.css} alt={item.id}/> ),
-        href: function() { return (
-          <a href={context.generateHref(item.id) } >
-            {this.svg}
-          </a> )
-        },
-        input: ( <input /> ),
-        button: ( <button type="submit"> {item.button} </button>)
-      }
-
-      return (
-        <li onClick={this.handleClick} id={item.id} key={item.id}>
-          { something[item.svg]  || 'lll'}
-        </li>
+    if(display === 'default') {
+      navItemDisplay = (
+        <ul className="navbar-items default">
+          <li onClick={this.handleClick} id='menu'   ><img src="svgs/menu-icon.svg" alt='menu'/></li>
+          <li onClick={this.handleClick} id='default'><img src="svgs/home-icon.svg" alt='home'/></li>
+          <li onClick={this.handleClick} id='search' ><img src="svgs/search-icon.svg" alt='search'/></li>
+          <li onClick={this.handleClick} id='default'>
+            <a href={`mailto:info@centronlp.com?subject=Centron%20Life%20Products%20-%20${'hey'}&body=Thanks%20dasds%0A%0ANewParagraph`}>
+              <img className="circle-btn" src="svgs/email-icon.svg" alt='mail' />
+            </a>
+          </li>
+          <li onClick={this.handleClick} id='default'>
+            <a href={`https://twitter.com/intent/tweet?text=Centron%20Life%20Products%20-%20Clavicle%20Supprt%20On%20Sale&url=https%3A%2F%2Fwww.centronlp.com%2Fclavicle%2Fhand&tsrc=twtr`}>
+              <img className="circle-btn" src="svgs/twitter-icon.svg" alt='twitter'/>
+            </a>
+          </li>
+        </ul>
       )
-    })
+    }
+
+    if(display === 'search') {
+      navItemDisplay = (
+        <ul className="navbar-items search">
+          <li id='menu'><img src="svgs/search-icon.svg" alt='search'/></li>
+          <li><input value={this.state.searchInput} onChange={this.handleChange} ref={(input) => { this.textInput = input; }} /></li>
+          <li onClick={this.handleClick} id='find'><button> Find </button></li>
+          <li onClick={this.handleClick} id='default'><button> X </button></li>
+        </ul>
+      )
+    }
 
     return (
       <div>
         { this.state.showMenu ? <MenuMain show={this.state.showMenu}/> : '' }
-
         <nav>
-          <ul className="navbar-items">
-            {navItemDisplay}
-          </ul>
+          {navItemDisplay}
         </nav>
       </div>
     )
